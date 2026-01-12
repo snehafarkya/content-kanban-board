d5490415ddff76043886cf6bdea8e8437244e6ec
snehafarkya <snhafarkya@gmail.com>
2026-01-12 14:21:34 +0530

    UI updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 
 export default function Header({setTasks}) {
   return (
-    <header className="flex flex-col md:flex-row justify-between items-center md:items-start  ">
-      <div className="flex flex-col">
+    <header className="flex flex-col md:flex-row justify-between items-center md:items-start mt-6 md:mt-0 ">
+      <div className="flex flex-col justify-center items-center md:items-start">
       <h1 className="text-4xl font-bold  tracking-tight">
         Content Pipeline
       </h1>
